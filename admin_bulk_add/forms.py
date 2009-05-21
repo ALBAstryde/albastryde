7ab582b8a896c91b451e7116df05e1839e867e11
johanneswilm <mail@johanneswilm.org>
2009-05-21 16:48:43 +0000

    bulk add working

--- admin_bulk_add/forms.py
+++ admin_bulk_add/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from lluvia.models import EstacionDeLluvia
+from precios.models import Mercado
 
 import datetime
 
@@ -9,12 +10,26 @@ date_inputformats=['%d.%m.%Y','%d/%m/%Y','%Y-%m-%d']
 ANO_CHOICES=[]
 
 for i in range (datetime.date.today().year,1920,-1):
-          ANO_CHOICES.append((i,i))
+	ANO_CHOICES.append((i,i))
 
-MES_CHOICES=((1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9),(10,10),(11,11),(12,12))
+MES_CHOICES=[]
+
+for i in range(1,12):
+	MES_CHOICES.append((i,i))
+
+DIA_CHOICES=[]
+
+for i in range(1,31):
+	DIA_CHOICES.append((i,i))
 
 
 class LluviaParameterForm(forms.Form):
 	estacion_de_lluvia=forms.ModelChoiceField(queryset=EstacionDeLluvia.objects.all(), required=True)
+	mes=forms.ChoiceField(required=True,choices=MES_CHOICES)
+	ano=forms.ChoiceField(required=True,choices=ANO_CHOICES)
+
+class PreciosParameterForm(forms.Form):
+	mercado=forms.ModelChoiceField(queryset=Mercado.objects.all(), required=True)
+	dia=forms.ChoiceField(required=True,choices=DIA_CHOICES)
 	mes=forms.ChoiceField(required=True,choices=MES_CHOICES)
 	ano=forms.ChoiceField(required=True,choices=ANO_CHOICES)
--- admin_bulk_add/views.py
+++ admin_bulk_add/views.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
-from admin_bulk_add.forms import LluviaParameterForm
+from admin_bulk_add.forms import LluviaParameterForm, PreciosParameterForm
 
 import calendar
 from django import forms
 from django.forms.models import modelformset_factory
 from lluvia.models import Prueba as LluviaPrueba
+from precios.models import Prueba as PreciosPrueba
+from precios.models import Mercado, Producto
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
 from django.template import RequestContext
@@ -19,36 +21,117 @@ def render_to_html(request,template,variables):
 	new_variables=variables
         return render_to_response(template, new_variables,context_instance=RequestContext(request))
 
-#class LluviaPruebaForm(forms.ModelForm):
-#	class Meta:
-#		model=LluviaPrueba
-
-
 def lluvia(request,ano=None,mes=None,estacion_de_lluvia=None):
+	app_label="lluvia"
+	model_label_plural="pruebas"
+	add_set_label="mes de lluvia"
 	if ano == None:
 		if ('estacion_de_lluvia' in request.GET) and request.GET['estacion_de_lluvia'].strip():
                 	estacion_de_lluvia = request.GET['estacion_de_lluvia']
                 	ano = request.GET['ano']
                 	mes = request.GET['mes']
-			return HttpResponseRedirect("/admin/lluvia/bulk_add/"+estacion_de_lluvia+"/"+ano+"/"+mes+"/")
+			return HttpResponseRedirect("/admin/lluvia/prueba/bulk_add/"+estacion_de_lluvia+"/"+ano+"/"+mes+"/")
 		else:
 			form=LluviaParameterForm()
-			return render_to_html(request,"bulk_add_form.html", {'form':form})
+			return render_to_html(request,"bulk_add_form_firstpage.html", {'form':form,'app_label':app_label,'model_label_plural':model_label_plural,'add_set_label':add_set_label})
 	else:
-		queryset=LluviaPrueba.objects.filter(fecha__year=int(ano)).filter(fecha__month=int(mes)).filter(estacion=int(estacion_de_lluvia))
-		if len(queryset) > 0:
-			LluviaPruebaFormSet = modelformset_factory(LluviaPrueba,extra=(calendar.monthrange(int(ano),int(mes))[1]-len(queryset)))
-			form = LluviaPruebaFormSet(queryset=queryset)
+		next_set_description="Grabar y añadir proximo mes"
+		if request.method == "POST":
+			LluviaPruebaFormSet = modelformset_factory(LluviaPrueba)
+			data = request.POST.copy()
+			form = LluviaPruebaFormSet(data=data)
+			if form.is_valid():
+				form.save()
+			else:
+				return render_to_html(request,"bulk_add_form.html", {"form":form,"next_set_description":next_set_description,"app_label":app_label,"model_label_plural":model_label_plural,"add_set_label":add_set_label})
+			if data.has_key('_next_set'):
+				mes=int(mes)
+				ano=int(ano)
+				if mes < 12:
+					mes += 1
+				else:
+					ano += 1
+					mes = 1
+				mes=str(mes)
+				ano=str(ano)
+				return HttpResponseRedirect("/admin/lluvia/prueba/bulk_add/"+estacion_de_lluvia+"/"+ano+"/"+mes+"/")
+			elif data.has_key('_continue_editing'):
+				return HttpResponseRedirect("/admin/lluvia/prueba/bulk_add/"+estacion_de_lluvia+"/"+ano+"/"+mes+"/")
+			else:
+				return HttpResponseRedirect("/admin/lluvia/")
 		else:
-			initial_list=[]
-			for day in range(1,calendar.monthrange(int(ano),int(mes))[1]+1):
-				initial_list.append({'estacion':str(estacion_de_lluvia),'fecha':str(ano)+'-'+str(mes)+'-'+str(day)})
-			LluviaPruebaFormSet = modelformset_factory(LluviaPrueba,extra=len(initial_list))
-			form = LluviaPruebaFormSet(initial=initial_list,queryset=LluviaPrueba.objects.none())
-		return render_to_html(request,"bulk_add_form_lluvia.html", {"form":form})
+			queryset=LluviaPrueba.objects.filter(fecha__year=int(ano)).filter(fecha__month=int(mes)).filter(estacion=int(estacion_de_lluvia))
+			if len(queryset) > 0:
+				LluviaPruebaFormSet = modelformset_factory(LluviaPrueba,extra=(calendar.monthrange(int(ano),int(mes))[1]-len(queryset)))
+				form = LluviaPruebaFormSet(queryset=queryset)
+			else:
+				initial_list=[]
+				for dia in range(1,calendar.monthrange(int(ano),int(mes))[1]+1):
+					if mes > 9:
+						mes_str=str(mes)
+					else:
+						mes_str="0"+str(mes)
+					if dia > 9:
+						dia_str=str(dia)
+					else:
+						dia_str="0"+str(dia)
+					initial_list.append({'estacion':str(estacion_de_lluvia),'milimetros_de_lluvia':'0.0','fecha':str(ano)+'-'+mes_str+'-'+dia_str})
+				LluviaPruebaFormSet = modelformset_factory(LluviaPrueba,extra=len(initial_list))
+				form = LluviaPruebaFormSet(initial=initial_list,queryset=LluviaPrueba.objects.none())
+			return render_to_html(request,"bulk_add_form.html", {"form":form,"next_set_description":next_set_description,"app_label":app_label,"model_label_plural":model_label_plural,"add_set_label":add_set_label})
 
-
-
-def precios(request,tag_name=None):
-	return render_to_html(request,"bulk_add_form.html", {})
-
+def precios(request,ano=None,mes=None,dia=None,mercado=None):
+	app_label="precios"
+	model_label_plural="pruebas"
+	add_set_label="precios de una fecha en un mercado"
+	if ano == None:
+		if ('mercado' in request.GET) and request.GET['mercado'].strip():
+                	mercado = request.GET['mercado']
+                	ano = request.GET['ano']
+                	mes = request.GET['mes']
+                	dia = request.GET['dia']
+			return HttpResponseRedirect("/admin/precios/prueba/bulk_add/"+mercado+"/"+ano+"/"+mes+"/"+dia+"/")
+		else:
+			form=PreciosParameterForm()
+			return render_to_html(request,"bulk_add_form_firstpage.html", {'form':form,'app_label':app_label,'model_label_plural':model_label_plural,'add_set_label':add_set_label})
+	else:
+		next_set_description="Grabar y añadir datos del proximo mercado"
+		if request.method == "POST":
+			PreciosPruebaFormSet = modelformset_factory(PreciosPrueba)
+			data = request.POST.copy()
+			form = PreciosPruebaFormSet(data=data)
+			if form.is_valid():
+				form.save()
+			else:
+				return render_to_html(request,"bulk_add_form.html", {"form":form,"next_set_description":next_set_description,"app_label":app_label,"model_label_plural":model_label_plural,"add_set_label":add_set_label})
+			if data.has_key('_next_set'):
+				mercado=int(mercado)
+				if mercado < len(Mercado.objects.all()):
+					mercado = 1
+				mercado=str(mercado)
+				return HttpResponseRedirect("/admin/precios/prueba/bulk_add/"+mercado+"/"+ano+"/"+mes+"/"+dia+"/")
+			elif data.has_key('_continue_editing'):
+				return HttpResponseRedirect("/admin/precios/prueba/bulk_add/"+mercado+"/"+ano+"/"+mes+"/"+dia+"/")
+			else:
+				return HttpResponseRedirect("/admin/precios/")
+		else:
+			queryset=PreciosPrueba.objects.filter(fecha__year=int(ano)).filter(fecha__month=int(mes)).filter(fecha__day=int(dia)).filter(mercado=int(mercado))
+			productos=Producto.objects.all()
+			if len(queryset) > 0:
+				PreciosPruebaFormSet = modelformset_factory(PreciosPrueba,extra=(len(productos)-len(queryset)))
+				form = LluviaPruebaFormSet(queryset=queryset)
+			else:
+				initial_list=[]
+				if mes > 9:
+					mes_str=str(mes)
+				else:
+					mes_str="0"+str(mes)
+				if dia > 9:
+					dia_str=str(dia)
+				else:
+					dia_str="0"+str(dia)
+				for producto in productos.iterator():
+					initial_list.append({'mercado':str(mercado),'producto':str(producto.id),'fecha':str(ano)+'-'+mes_str+'-'+dia_str})
+				PreciosPruebaFormSet = modelformset_factory(PreciosPrueba,extra=len(initial_list))
+				form = PreciosPruebaFormSet(initial=initial_list,queryset=PreciosPrueba.objects.none())
+			return render_to_html(request,"bulk_add_form.html", {"form":form,"next_set_description":next_set_description,"app_label":app_label,"model_label_plural":model_label_plural,"add_set_label":add_set_label})
