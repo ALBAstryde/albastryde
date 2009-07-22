12ef00ca55548c922038d5ddb69e16032a862dad
johanneswilm <mail@johanneswilm.org>
2009-07-22 18:59:03 +0000

    add semilla admin.py

---
+++ semilla/admin.py
@@ -0,0 +1,7 @@
+from semilla.models import Semilla, Producto, Productor, Variedad
+from django.contrib import admin
+
+admin.site.register(Semilla)
+admin.site.register(Producto)
+admin.site.register(Productor)
+admin.site.register(Variedad)
