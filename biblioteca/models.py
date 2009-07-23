7a18d7becd977f8aae8ba2291188b11a4d6ec6a3
johanneswilm <mail@johanneswilm.org>
2009-07-23 01:07:39 +0000

    biblioteca models reworked

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -2,7 +2,7 @@
 
 from django.db import models
 from django.conf import settings
-import datetime
+from djutils.features.fuzzydate import FuzzyDateField
 
           
 class Autor(models.Model):
@@ -18,14 +18,13 @@ class Editorial(models.Model):
 		return self.nombre
 
 class PalabraClave(models.Model):
-	nombre = models.CharField(primary_key=True)
+	nombre = models.CharField(primary_key=True,max_length=50)
 
-class Biblioteca(models.Model):
-	#content_type = models.CharField(max_length=5, blank=True, null=True)
+class Documento(models.Model):
 	nombre = models.CharField(max_length=200, help_text="Por favor ingrese le titulo del documento", verbose_name="Titulos")
 	autor = models.ForeignKey(Autor, verbose_name="Autor")
-	publicado = models.FuzzyDateField(choices=ANOS_CHOICES, verbose_name="Año de publicacion")
-	fecha = models.DateTimeField(editable=False, auto_now_add=True)
+	fecha_publicado = FuzzyDateField(verbose_name="Fecha de publicacion")
+	fecha_anadido = models.DateTimeField(editable=False, auto_now_add=True, verbose_name="Fecha añadido")
 	descripcion = models.TextField(max_length=200, verbose_name="Descripcion")
 	organizacion = models.ForeignKey(Editorial, verbose_name="Organizacion")
 	palabras_claves = models.ManyToManyField(PalabraClave)
