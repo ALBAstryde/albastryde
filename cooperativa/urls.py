f1894e19206912a15359402339ee8ec3af5fff91
crocha <Rocha>
2009-08-18 20:33:45 +0000

    adding templates app cooperativas

---
+++ cooperativa/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls.defaults import *
+from models import Cooperativa, Detallecoop
+
+#info = {
+#	'queryset': Cooperativa.objects.all(),
+	#'queryset': Detallecoop.objects.all(),
+#}
+
+#info_dict = {
+#	'queryset': Detallecoop.objects.all(),
+#}
+
+#urlpatterns = patterns('django.views.generic.list_detail',
+#	url(r'^$', 'object_list', info, name='cooperativa-list'),
+#	url(R'^(?P<object_id>\d+)/$', 'object_detail', info, name='cooperativa-detail'),
+#	url(r'^$', 'object_list', info, name='Detallecoop-list'),
+#	url(R'^(?P<object_id>\d+)/$', 'object_detail', info, name='Detallecoop-detail'),
+#)
+
+urlpatterns = patterns('mapamagfor.cooperativa.views',
+	(r'^index/$', 'index'),
+	(r'^listar/(?P<listar_id>\d+)/$', 'cooperativar'),
+	(r'^detalle/(?P<deta_id>\d+)/$', 'detallelistar'),
+	#(r'^departamento/$', 'comeindex'),
+)
+
--- cooperativa/views.py
+++ cooperativa/views.py
@@ -1,1 +1,30 @@
-# Create your views here.
+# -*- encoding: utf-8 -*-
+from django.http import Http404
+from django.shortcuts import render_to_response
+from cooperativa.models import *
+from lugar.models import *
+from forms import *
+
+def index(request):
+	s = Cooperativa.objects.all()
+	return render_to_response("base.html", { 's':s })
+
+def cooperativar(request, listar_id):
+	lista = Cooperativa.objects.get(pk=listar_id)
+	return render_to_response("cooperativa/cooperativa_detail.html", {'lista':lista})
+
+def detallelistar(request, deta_id):
+	p = Detallecoop.objects.filter(cooperativa=deta_id)
+	#if k = p.cooperativa == deta_id
+	return render_to_response("cooperativa/cooperativa_list.html", {'p': p})
+
+#def comeindex(request):
+#	F = FormLugar(request.GET)
+#	query = request.GET.get('departamentos', '')
+#	if query:
+#		j = Municipio.objects.filter(departamento=query)
+#		return render_to_response("cooperativa/index.html", {'j':j})
+#	else:
+#		k = Departamento.objects.all()
+#		return render_to_response("cooperativa/index.html", locals())
+
