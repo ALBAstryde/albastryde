3e37f232dee05edb8ce5257568e955e20db8b222
johanneswilm <mail@johanneswilm.org>
2009-08-18 21:22:59 +0000

    integrating cooperativas part 2

--- cooperativas/lugar/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from mapamagfor.cooperativa.models import Cooperativa, Detallecoop
-
-urlpatterns = patterns('mapamagfor.lugar.views',
-	(r'^departamento/$', 'comeindex'),
-	(r'^cooperativa/(?P<mun_id>\d+)/$', 'coopmun'),
-	(r'^ver/(?P<deta_id>\d+)/$', 'verdetalle'),
-	(r'^detallec/(?P<p_id>\d+)/$', 'detacompleto'),
-)
--- cooperativas/views.py
+++ cooperativas/views.py
@@ -16,16 +16,16 @@ def render_to_html(request,template,variables):
 
 def index(request):
 	s = Cooperativa.objects.all()
-	return render_to_html(request,"base.html", { 's':s })
+	return render_to_html(request,"index.html", { 's':s })
 
 def cooperativar(request, listar_id):
 	lista = Cooperativa.objects.get(pk=listar_id)
-	return render_to_html(request,"cooperativa/cooperativa_detail.html", {'lista':lista})
+	return render_to_html(request,"cooperativa_detail.html", {'lista':lista})
 
 def detallelistar(request, deta_id):
 	p = Detallecoop.objects.filter(cooperativa=deta_id)
 	#if k = p.cooperativa == deta_id
-	return render_to_html(request,"cooperativa/cooperativa_list.html", {'p': p})
+	return render_to_html(request,"cooperativa_list.html", {'p': p})
 
 #def comeindex(request):
 #	F = FormLugar(request.GET)
