8e975ac33ce38dfc1ac3ea46f7d0604721b421a3
johanneswilm <mail@johanneswilm.org>
2009-08-24 16:38:45 +0000

    moved biblioteca templates to their folder

--- biblioteca/views.py
+++ biblioteca/views.py
@@ -17,7 +17,7 @@ def palabra_clave(request,pk):
 	a = Documento.objects.order_by('-fecha_anadido')[:5]
 	palabra_clave=PalabraClave.objects.get(pk=pk)
 	documentos=palabra_clave.documento_set.all()
-        return render_to_html(request,"/biblioteca_palabra_clave.html", {
+        return render_to_html(request,"/biblioteca/biblioteca_palabra_clave.html", {
 			"palabra_clave": palabra_clave,
 			"all_palabras_claves": all_palabras_claves,
 			"documentos": documentos,
@@ -28,7 +28,7 @@ def palabra_clave(request,pk):
 def index(request):
 	all_palabras_claves=PalabraClave.objects.all()	
 	a = Documento.objects.order_by('-fecha_anadido')[:5]
-        return render_to_html(request,"/biblioteca.html", {
+        return render_to_html(request,"/biblioteca/biblioteca.html", {
 			"all_palabras_claves": all_palabras_claves,
         		"a": a
 		}
@@ -38,7 +38,7 @@ def detalle(request, libro):
 	all_palabras_claves=PalabraClave.objects.all()	
 	a = Documento.objects.order_by('-fecha_anadido')[:5]
 	resultado = Documento.objects.get(pk=libro)
-	return render_to_html(request,"/biblioteca_detalle.html", {
+	return render_to_html(request,"/biblioteca/biblioteca_detalle.html", {
 		"all_palabras_claves": all_palabras_claves,
 	       	"results": resultado,
         	"a": a
