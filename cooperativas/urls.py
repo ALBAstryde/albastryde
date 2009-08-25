55c658aeb9574fb3907ed205532ea5b6e63a6676
crocha <Rocha>
2009-08-25 02:35:41 +0000

    some fixed templates cooperativas

--- cooperativas/urls.py
+++ cooperativas/urls.py
@@ -1,26 +1,11 @@
 from django.conf.urls.defaults import *
 from cooperativas.models import Cooperativa, Detallecoop
 
-#info = {
-#	'queryset': Cooperativa.objects.all(),
-	#'queryset': Detallecoop.objects.all(),
-#}
 
-#info_dict = {
-#	'queryset': Detallecoop.objects.all(),
-#}
-
-#urlpatterns = patterns('django.views.generic.list_detail',
-#	url(r'^$', 'object_list', info, name='cooperativa-list'),
-#	url(R'^(?P<object_id>\d+)/$', 'object_detail', info, name='cooperativa-detail'),
-#	url(r'^$', 'object_list', info, name='Detallecoop-list'),
-#	url(R'^(?P<object_id>\d+)/$', 'object_detail', info, name='Detallecoop-detail'),
-#)
 
 urlpatterns = patterns('cooperativas.views',
 	(r'^$', 'index'),
 	(r'^listar/(?P<listar_id>\d+)/$', 'cooperativar'),
 	(r'^detalle/(?P<deta_id>\d+)/$', 'detallelistar'),
-	#(r'^departamento/$', 'comeindex'),
 )
 
--- cooperativas/views.py
+++ cooperativas/views.py
@@ -2,21 +2,18 @@
 from django.http import Http404
 from coffin.shortcuts import render_to_response
 from django.template import RequestContext
-
 from cooperativas.models import *
 from lugar.models import *
 
-#from forms import *
 
 def render_to_html(request,template,variables):
         variables['request']=request
         new_variables=variables
         return render_to_response(template, new_variables,context_instance=RequestContext(request))
 
-
 def index(request):
 	s = Cooperativa.objects.all()
-	return render_to_html(request,"/cooperativas/base.html", { 's':s })
+	return render_to_html(request,"/cooperativas/listado.html", { 's':s })
 
 def cooperativar(request, listar_id):
 	lista = Cooperativa.objects.get(pk=listar_id)
@@ -24,16 +21,4 @@ def cooperativar(request, listar_id):
 
 def detallelistar(request, deta_id):
 	p = Detallecoop.objects.filter(cooperativa=deta_id)
-	#if k = p.cooperativa == deta_id
 	return render_to_html(request,"/cooperativas/cooperativa_list.html", {'p': p})
-
-#def comeindex(request):
-#	F = FormLugar(request.GET)
-#	query = request.GET.get('departamentos', '')
-#	if query:
-#		j = Municipio.objects.filter(departamento=query)
-#		return render_to_response("cooperativas/coop_index.html", {'j':j})
-#	else:
-#		k = Departamento.objects.all()
-#		return render_to_response("cooperativas/coop_index.html", locals())
-
